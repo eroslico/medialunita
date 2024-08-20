b779e118e37514f7bd2c063f8ecdc3bdc34f77dc
Eros Lico <eroslico1@gmailcom>
2024-08-20 14:24:11 -0300

    auth fully

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -2,10 +2,15 @@
 
 import Link from "next/link";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faMugHot } from "@fortawesome/free-solid-svg-icons";
-import { signIn } from "next-auth/react";
+import { faMugHot, faUser } from "@fortawesome/free-solid-svg-icons";
+import { signIn, signOut } from "next-auth/react";
+import { Session } from "next-auth";
+import { parseFullName } from "parse-full-name";
+import Image from "next/image";
 
-export default function Header() {
+export default function Header({ session }: { session: Session | null }) {
+  const name = session?.user?.name || "";
+  const { first: firstName } = parseFullName(name);
   return (
     <>
       <header className="mb-16">
@@ -19,15 +24,36 @@ export default function Header() {
             <Link href={"/about"}>FAQ</Link>
             <Link href={"/about"}>Contact</Link>
             <div className="flex gap-4">
-              <button
-                onClick={() => signIn("google")}
-                className="border-2 rounded-full px-4 py-2 ml-4"
-              >
-                Login
-              </button>
-              <button className="bg-yellow-300 rounded-full px-4 py-2">
-                Sign up
-              </button>
+              {session && (
+                <div className="">
+                  <button
+                    onClick={() => signOut()}
+                    className="flex items-center gap-2 bg-yellow-300 rounded-full p-1 pr-4"
+                  >
+                    <Image
+                      src={session.user?.image as string}
+                      alt={"avatar"}
+                      width="30"
+                      height="30"
+                      className="rounded-full"
+                    />
+                    {firstName}
+                  </button>
+                </div>
+              )}
+              {!session && (
+                <>
+                  <button
+                    onClick={() => signIn("google")}
+                    className="border-2 rounded-full px-4 py-2 ml-4"
+                  >
+                    Login
+                  </button>
+                  <button className="bg-yellow-300 rounded-full px-4 py-2">
+                    Sign up
+                  </button>
+                </>
+              )}
             </div>
           </nav>
         </div>
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Header from "./components/Header";
+import { getServerSession } from "next-auth";
+import { authOptions } from "@/lib/authOptions";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -15,10 +17,11 @@ export default async function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const session = await getServerSession(authOptions);
   return (
     <html lang="en">
       <body className={inter.className}>
-        <Header />
+        <Header session={session} />
 
         {children}
       </body>
